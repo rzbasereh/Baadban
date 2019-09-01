e863ff6031ef18823a6332838bed077381f6dc6d
RezaBasereh <rzbasereh1@gmail.com>
2019-09-01 16:07:33 +0430

    create news and part of about section

--- js/script.js
+++ js/script.js
@@ -1,6 +1,23 @@
 $(window).on("load", function () {
-
+    // $(".owl-item").width();
+    $(".owl-prev").html("<i class='fas fa-chevron-left fa-lg'></i>");
+    $(".owl-next").html("<i class='fas fa-chevron-right fa-lg'></i>");
 });
 $(document).ready(function () {
-
+    $('.owl-carousel').owlCarousel({
+        loop:false,
+        margin:10,
+        nav:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:2
+            },
+            1000:{
+                items:2
+            }
+        }
+    });
 });
