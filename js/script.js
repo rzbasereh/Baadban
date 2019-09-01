e985be975de5ace0f92b8e6dbb689b004e6e5c80
RezaBasereh <rzbasereh1@gmail.com>
2019-09-02 01:36:40 +0430

    create samples part and edit other sections

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ $(window).on("load", function () {
     // $(".owl-item").width();
     $(".owl-prev").html("<i class='fas fa-chevron-left fa-lg'></i>");
     $(".owl-next").html("<i class='fas fa-chevron-right fa-lg'></i>");
+
 });
 $(document).ready(function () {
     $('.owl-carousel').owlCarousel({
@@ -12,12 +13,17 @@ $(document).ready(function () {
             0:{
                 items:1
             },
-            600:{
-                items:2
-            },
-            1000:{
+            750:{
                 items:2
             }
         }
+    });
+
+    $("section#news .owl-carousel .item").hover(function () {
+        $(this).find(".item-title").css("height", "100%");
+        $(this).find(".item-title").children("span").addClass("center-abs");
+    }, function () {
+        $(this).find(".item-title").css("height", "fit-content");
+        $(this).find(".item-title").children("span").removeClass("center-abs");
     });
 });
